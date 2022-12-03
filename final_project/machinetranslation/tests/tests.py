f5f09b3c6a02250690f818c01a61665a8bd285d3
alamot <antalamot@gmail.com>
2022-12-03 12:35:44 +0200

    tests additions

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -10,19 +10,27 @@ class TestTranslator(unittest.TestCase):
     English to French Translation Test
     """
     def test_when_null_en2fr(self):
+        print("Testing english_to_french for null")
         self.assertEqual(english_to_french(""),"")
 
     def test_word_hello_en2fr(self):
+        print("Testing english_to_french with assertEqual")
         self.assertEqual(english_to_french('Hello'),'Bonjour')
+        print("Testing english_to_french with assertNotEqual")
+        self.assertNotEqual(english_to_french("Bonjour"), "Hello")
 
     """
     French to English Translation Test
     """
     def test_when_null_fr2en(self):
+        print("Testing french_to_english for null")
         self.assertEqual(french_to_english(""),"")
 
     def test_word_hello_fr2en(self):
+        print("Testing french_to_english with assertEqual")
         self.assertEqual(french_to_english('Bonjour'),'Hello')
+        print("Testing french_to_english with assertNotEqual")
+        self.assertNotEqual(french_to_english("Hello"), "Bonjour")
 
 
 unittest.main()
